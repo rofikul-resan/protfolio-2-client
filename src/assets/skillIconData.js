f691788bf14a0fa591ae5e8f85c4c5b46995f2c6
rofikul-resan <resan6231@gmail.com>
2024-08-06 20:13:17 +0600

    skill icon data genetare

---
+++ src/assets/skillIconData.js
@@ -0,0 +1,21 @@
+const skillImgDataObject = {
+  HTML: "/public/html-5.png",
+  CSS: "/public/css-3.png",
+  JavaScript: "/public/javascript.svg",
+  React: "/public//react.png",
+  NodeJs: "/public/node-js.png",
+  Express: "/public/express-js.svg",
+  MongoDB: "/public/mongodb.svg",
+
+  Git: "/public/git.svg",
+  GitHub: "/public/github.png",
+  TailwindCSS: "/public/tailwindcss.svg",
+  Bootstrap: "/public/bootstrap.svg",
+  TypeScript: "/public/typescript.svg",
+  Sass: "/public/icons8-sass.svg",
+  MaterialUI:
+    "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  Redux: "/public/redux-50.png",
+};
+
+export default skillImgDataObject;
