a9bc2cdb79d06f2fa8daca51a8c7f6aa92507155
rofikul-resan <resan6231@gmail.com>
2025-01-21 00:22:05 +0600

    make responsible skill section

--- src/components/card/SkillIconCard.jsx
+++ src/components/card/SkillIconCard.jsx
@@ -1,20 +1,24 @@
 const SkillIconCard = ({ skill, size }) => {
-  console.log(size);
-
   return (
     <div>
-      <div className={`relative h-28 w-28 my-4 pt-2`}>
+      <div
+        className={`relative my-4 pt-2`}
+        style={{ height: size + "px", width: `${size}px` }}
+      >
         <div
-          className={`h-[${size}px] w-[${size}px] border-cyan-200 border-1  absolute bg-sky-200 z-0 rounded-md  rotate-45  `}
+          className={`h-full w-full border-cyan-200 border-1  absolute bg-sky-200 z-0 rounded-md  rotate-45  `}
         ></div>
         <div className="mt-2">
           <img
             src={skill.icon}
             alt={skill.icon}
-            className="h-14 w-14 mx-auto relative z-10"
+            className="h-1/2 w-1/2 mx-auto relative z-10"
           />
           <div className="relative z-10 ">
-            <p className="text-center font-semibold text-gray-900">
+            <p
+              className="text-center font-semibold text-gray-900 "
+              style={{ fontSize: `${size / 10}px` }}
+            >
               {skill.name}
             </p>
           </div>
--- src/pages/home/section/SkillSection.jsx
+++ src/pages/home/section/SkillSection.jsx
@@ -6,22 +6,41 @@ import SkillIconCard from "../../../components/card/SkillIconCard";
 const SkillSection = () => {
   const [allSkills, setAllSkills] = useState([]);
   const [cardSize, setCardSize] = useState(0);
+  const [cardRowLimit, setCardRowLimit] = useState(0);
+  const [displayWidth, setDisplayWidth] = useState(innerWidth);
 
   useEffect(() => {
-    const displayWidth = screen.width;
-    setCardSize(displayWidth / 10.77);
+    window.addEventListener("resize", () => {
+      setDisplayWidth(innerWidth);
+    });
+  }, []);
+
+  useEffect(() => {
+    setCardSize(displayWidth < 1201 ? displayWidth / 12 : 1200 / 12);
+
+    setCardRowLimit(
+      displayWidth < 1201 ? Math.floor(displayWidth / 200) : 1200 / 200
+    );
+    console.log(cardRowLimit);
 
     fetch("/data/skillIcon.json")
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
         const allSkillsIconList = [];
         const len = data.length;
 
-        if (data.length > 6) {
+        if (data.length > cardRowLimit) {
+          console.log(cardRowLimit);
           let isOddRow = false;
-          for (let i = 0; i < len; isOddRow ? (i += 6) : (i += 7)) {
-            const row = data.slice(i, !isOddRow ? i + 6 : i + 7);
+          for (
+            let i = 0;
+            i < len;
+            isOddRow ? (i += cardRowLimit) : (i += cardRowLimit + 1)
+          ) {
+            const row = data.slice(
+              i,
+              !isOddRow ? i + cardRowLimit : i + cardRowLimit + 1
+            );
             isOddRow = !isOddRow;
             if (row.length > 0) {
               allSkillsIconList.push(row);
@@ -31,8 +50,7 @@ const SkillSection = () => {
         setAllSkills(allSkillsIconList);
         return;
       });
-  }, []);
-  console.log(cardSize);
+  }, [cardRowLimit, displayWidth]);
 
   return (
     <section>
@@ -42,13 +60,14 @@ const SkillSection = () => {
           {allSkills.length > 0 &&
             allSkills.map((skillRow, idx) => (
               <div
+                style={{
+                  marginTop: idx >= 1 ? -cardSize / 2 : 0,
+                }}
                 key={idx}
-                className={`flex  justify-center gap-12 ${
-                  idx >= 1 ? "-mt-14" : ""
-                } `}
+                className={`flex  justify-center gap-12  `}
               >
                 {skillRow.map((skill, i) => (
-                  <SkillIconCard key={i} skill={skill} size={127} />
+                  <SkillIconCard key={i} skill={skill} size={cardSize} />
                 ))}
               </div>
             ))}
